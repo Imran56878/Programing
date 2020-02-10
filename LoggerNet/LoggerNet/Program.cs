9bddcd90d315968ef0a49ead5c53cfe3e5d6d003
siddhivinayak08 <siddhivinup@gmail.com>
2020-02-10 18:13:49 +0530

    Logger

---
+++ LoggerNet/LoggerNet/Program.cs
@@ -0,0 +1,21 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[assembly: log4net.Config.XmlConfigurator(Watch=true)]
+
+namespace LoggerNet
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType );
+        static void Main(string[] args)
+        {
+            Console.Write("Hello!");
+            log.Error("This is my error message ");
+            Console.ReadLine();
+        }
+    }
+}
